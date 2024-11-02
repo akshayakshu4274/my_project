308da0bf22d42f032cd3d0fd637d5d97e1070fd2
akshay <90513482+akshayakshu4274@users.noreply.github.com>
2024-11-02 09:57:11 +0530

    Update password_validator.py

--- src/password_validator.py
+++ src/password_validator.py
@@ -1,1 +1,9 @@
+# password_validator.py
+import re
+
+def validate_passwords(input_passwords):
+    pattern = r'^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[$#@]).{6,12}$'
+    passwords = input_passwords.split(',')
+    valid_passwords = [pwd for pwd in passwords if re.match(pattern, pwd)]
+    return ','.join(valid_passwords)
 
