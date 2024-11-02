232d065b812999f4ca381b1311901af2dd87173c
akshay <90513482+akshayakshu4274@users.noreply.github.com>
2024-11-02 09:56:27 +0530

    Update word_frequency.py

--- src/word_frequency.py
+++ src/word_frequency.py
@@ -1,1 +1,9 @@
+# word_frequency.py
+def word_frequency(line):
+    words = line.split()
+    frequency = {}
+    for word in words:
+        frequency[word] = frequency.get(word, 0) + 1
+    sorted_frequency = sorted(frequency.items())
+    return sorted_frequency
 
