c41febf0d92ce905a0fec86978be621ca9ee65c9
akshay <90513482+akshayakshu4274@users.noreply.github.com>
2024-11-02 11:04:20 +0530

    Update test_word_frequency.py

--- tests/test_word_frequency.py
+++ tests/test_word_frequency.py
@@ -1,4 +1,4 @@
-from ..word_frequency import word_frequency
+from word_frequency import word_frequency
 
 def test_word_frequency():
     assert word_frequency("hello world hello") == [('hello', 2), ('world', 1)]
