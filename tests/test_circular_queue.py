b3a5413f23363cffb673c66c4f5295c7c5064b63
akshay <90513482+akshayakshu4274@users.noreply.github.com>
2024-11-02 10:01:09 +0530

    Update test_circular_queue.py

--- tests/test_circular_queue.py
+++ tests/test_circular_queue.py
@@ -1,1 +1,10 @@
+from circular_queue import CircularQueue
+
+def test_circular_queue():
+    cq = CircularQueue(max_length=3)
+    cq.enqueue("A")
+    cq.enqueue("B")
+    cq.enqueue("C")
+    cq.enqueue("D")
+    assert cq.display() == ["B", "C", "D"]
 
