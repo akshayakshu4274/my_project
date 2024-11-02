2ed60964252e90639612e21b4c0adfaffa494ecc
akshay <90513482+akshayakshu4274@users.noreply.github.com>
2024-11-02 09:56:52 +0530

    Update circular_queue.py

--- src/circular_queue.py
+++ src/circular_queue.py
@@ -1,1 +1,17 @@
+# circular_queue.py
+class CircularQueue:
+    def __init__(self, max_length=5):
+        self.queue = {}
+        self.max_length = max_length
+        self.current_index = 0
+    
+    def enqueue(self, element):
+        self.queue[self.current_index] = element
+        self.current_index += 1
+        if len(self.queue) > self.max_length:
+            oldest_index = min(self.queue.keys())
+            del self.queue[oldest_index]
+    
+    def display(self):
+        return list(self.queue.values())
 
